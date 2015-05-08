8a6bb6789c2f21177900da70b16833493cb5b704
Ivan Yelizariev <ivann.exe@gmail.com>
2015-05-08 12:26:29 +0200

    split mail_delete_odoo_footer in two modules:
    * mail_delete_access_link
    * mail_delete_sent_by_footer

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name' : 'Delete access link in email footer',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Social Network',
+    'website' : 'https://yelizariev.github.io',
+    'depends' : ['mail'],
+    'data':[
+        ],
+    'installable': True
+}
