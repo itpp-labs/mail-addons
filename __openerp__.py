133d940602eb10fb5a1ce2f9a66294b723721ce7
Ivan Yelizariev <ivann.exe@gmail.com>
2015-05-08 12:26:29 +0200

    split mail_delete_odoo_footer in two modules:
    * mail_delete_access_link
    * mail_delete_sent_by_footer

--- __openerp__.py
+++ __openerp__.py
@@ -1,10 +1,10 @@
 {
-    'name' : 'Delete "Sent by..." footer in email',
+    'name' : 'Delete Odoo footer in email',
     'version' : '1.0.0',
     'author' : 'Ivan Yelizariev',
     'category' : 'Social Network',
     'website' : 'https://yelizariev.github.io',
-    'depends' : ['mail'],
+    'depends' : ['mail_delete_access_link', 'mail_delete_sent_by_footer'],
     'data':[
         ],
     'installable': True
