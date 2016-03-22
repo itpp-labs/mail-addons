dfa270255e4a189d0fd6df8a34b615027bb4a062
x620 <romanchenko@it-projects.info>
2016-03-22 18:11:27 +0500

    Add mail to depends in __openerp__.py

--- res_partner_mails_count/__openerp__.py
+++ res_partner_mails_count/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail_sent'],
+    'depends': ['base', 'mail'],
 
     # always loaded
     'data': [
