3bafef5827b881426ef2a6a049557fbe87c067d6
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-05-04 15:58:43 +0500

    [FIX] steps

--- res_partner_mails_count/static/src/js/res_partner_mails_count_tour.js
+++ res_partner_mails_count/static/src/js/res_partner_mails_count_tour.js
@@ -25,6 +25,7 @@ odoo.define('res_partner_mails_count.res_partner_mails_count_tour', function (re
                 title: _t("Done"),
                 placement: 'top',
                 waitNot: '.mails_to:visible',
+                waitFor: '.o_mail_thread',
                 element: '.o_mail_thread',
                 content: _t("Message are found. <br/>Enjoy your day! <br/> <br/><a href='https://www.it-projects.info/apps' target='_blank'>IT-Projects LLC</a> team "),
                 popover: {next: _t("Close Tutorial")},
