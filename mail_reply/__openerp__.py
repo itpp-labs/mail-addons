533fb074639bc9f5170b6e33756cf937fe0da6c9
x620 <romanchenko@it-projects.info>
2016-05-05 11:15:30 +0500

    [DOC] change module descriptions

---
+++ mail_reply/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Show reply button""",
+    "summary": """Got a message out of a Form? Now you can reply to it too!""",
+    "category": "Discuss",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
