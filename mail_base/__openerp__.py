40a6b1f2c0bb4b1a475e01bc73195461c963c98e
Ivan Yelizariev <yelizariev@it-projects.info>
2016-05-09 16:27:27 +0500

    [DOC] price + tiny updates

--- mail_base/__openerp__.py
+++ mail_base/__openerp__.py
@@ -9,6 +9,8 @@
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "website": "https://it-projects.info",
     "license": "LGPL-3",
+    'price': 9.00,
+    'currency': 'EUR',
 
     "depends": [
         "base",
