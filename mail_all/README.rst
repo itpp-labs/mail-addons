ed7da6112debe2eaa6f93ec5313a2cc6ccd0ef71
x620 <romanchenko@it-projects.info>
2016-05-25 17:30:39 +0500

    [DOC] Init commit: docs, manifests and description

---
+++ mail_all/README.rst
@@ -0,0 +1,16 @@
+===================
+ Show all messages
+===================
+
+Adds ``Messaging / All`` menu, that shows all messages accesable by current user
+
+Further information
+-------------------
+
+HTML Description: https://apps.odoo.com/apps/modules/8.0/mail_all/
+
+Usage instructions: `<doc/index.rst>`_
+
+Changelog: `<doc/changelog.rst>`_
+
+Tested on Odoo 8.0 0af32f3f84bae07b11abb8538d02e35c7369a348
---
+++ mail_all/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Show all messages",
+    "summary": """Checkout all messages where you have access""",
+    "category": "Social Network",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "GPL-3",
+    "price": 40.00,
+    "currency": "EUR",
+
+    "depends": [
+        "mail",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        # "views/templates.xml",
+    ],
+    "qweb": [
+        # "static/src/xml/menu.xml",
+    ],
+    "demo": [],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+}
