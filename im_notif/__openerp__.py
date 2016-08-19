a25574d56bb7afed6ee5eabd0ee799d6da893c3f
Ivan Yelizariev <yelizariev@it-projects.info>
2016-08-19 19:26:55 +0500

    fix lint errors

--- im_notif/__openerp__.py
+++ im_notif/__openerp__.py
@@ -2,10 +2,10 @@
 {
     'name': 'IM Notifications',
     'version': '1.0.1',
-    'author' : 'IT-Projects LLC, Ivan Yelizariev',
+    'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
-    'category' : 'Tools',
-    'website' : 'https://twitter.com/yelizariev',
+    'category': 'Tools',
+    'website': 'https://twitter.com/yelizariev',
     'price': 9.00,
     'currency': 'EUR',
     'depends': ['im_chat', 'mail'],
@@ -13,7 +13,7 @@
     'data': [
         'im_notif_data.xml',
         'im_notif_views.xml',
-        ],
+    ],
     'installable': False,
     'uninstall_hook': 'pre_uninstall',
 }
