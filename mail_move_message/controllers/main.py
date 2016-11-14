ac03864f9d8b2f4702312d2ed6a03aefbeccce02
Patrik Lermon <patrik.lermon@gmail.com>
2016-11-14 10:01:00 +0100

    [FIX] compatibility with 10.0

--- mail_move_message/controllers/main.py
+++ mail_move_message/controllers/main.py
@@ -3,10 +3,10 @@ from openerp.addons.web.controllers.main import DataSet
 from openerp.tools.translate import _
 from openerp import http
 from openerp.http import request
-import openerp
+from openerp.addons.bus.controllers.main import BusController
 
 
-class MailChatController(openerp.addons.bus.controllers.main.BusController):
+class MailChatController(BusController):
     # -----------------------------
     # Extends BUS Controller Poll
     # -----------------------------
