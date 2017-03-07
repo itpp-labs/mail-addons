0bf6acd17a73ac2d7f1d02409629a05aaa5e00ec
ilmir <ilmir.karamov@gmail.com>
2017-03-07 13:59:16 +0500

    [ADD] new version for mail_move_message

--- mail_move_message/__openerp__.py
+++ mail_move_message/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Mail relocation',
-    'version': '1.0.4',
+    'version': '1.0.5',
     'author': 'IT-Projects LLC, Ivan Yelizariev, Pavel Romanchenko',
     'license': 'LGPL-3',
     'category': 'Discuss',
