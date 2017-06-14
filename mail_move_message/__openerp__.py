fee5b8714bd2d485fc6b263c0ff605912ffe4bb1
ilmir <ilmir.karamov@gmail.com>
2017-06-14 13:05:53 +0500

    [DOC] remove extra images line from __openerp__.py

--- mail_move_message/__openerp__.py
+++ mail_move_message/__openerp__.py
@@ -11,7 +11,6 @@
     'price': 100.00,
     'currency': 'EUR',
     'depends': ['mail_all', 'web_polymorphic_field'],
-    'images': ['images/inbox.png'],
     'data': [
         'mail_move_message_views.xml',
         'data/mail_move_message_data.xml',
