bcf643ce87101888504686bfa0cdbe4764d26c3d
ArtyomLosev <losev@it-projects.info>
2017-06-19 15:25:26 +0500

    [FIX] __openerp__ version

--- mail_base/__openerp__.py
+++ mail_base/__openerp__.py
@@ -4,7 +4,7 @@
     "summary": """Makes Mail extendable""",
     "category": "Discuss",
     "images": [],
-    "version": "1.0.1",
+    "version": "1.0.2",
 
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "support": "apps@it-projects.info",
