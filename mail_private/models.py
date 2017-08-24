5687f334a14cff1659b5b3a6e684c98559497acb
ArtyomLosev <losev@it-projects.info>
2017-08-24 16:19:44 +0500

    [FIX] mail_private full composer fix; few more major fixes

--- mail_private/models.py
+++ mail_private/models.py
@@ -1,9 +1,81 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
-    private = fields.Boolean(string='Send Internal Message')
+    is_private = fields.Boolean(string='Send Internal Message')
+
+    @api.multi
+    def send_mail(self, auto_commit=False):
+        """ Process the wizard content and proceed with sending the related
+            email(s), rendering any template patterns on the fly if needed. """
+        for wizard in self:
+            # Duplicate attachments linked to the email.template.
+            # Indeed, basic mail.compose.message wizard duplicates attachments in mass
+            # mailing mode. But in 'single post' mode, attachments of an email template
+            # also have to be duplicated to avoid changing their ownership.
+            if wizard.attachment_ids and wizard.composition_mode != 'mass_mail' and wizard.template_id:
+                new_attachment_ids = []
+                for attachment in wizard.attachment_ids:
+                    if attachment in wizard.template_id.attachment_ids:
+                        new_attachment_ids.append(attachment.copy({'res_model': 'mail.compose.message', 'res_id': wizard.id}).id)
+                    else:
+                        new_attachment_ids.append(attachment.id)
+                    wizard.write({'attachment_ids': [(6, 0, new_attachment_ids)]})
+
+            # Mass Mailing
+            mass_mode = wizard.composition_mode in ('mass_mail', 'mass_post')
+
+            Mail = self.env['mail.mail']
+            ActiveModel = self.env[wizard.model if wizard.model else 'mail.thread']
+            if wizard.template_id:
+                # template user_signature is added when generating body_html
+                # mass mailing: use template auto_delete value -> note, for emails mass mailing only
+                Mail = Mail.with_context(mail_notify_user_signature=False)
+                ActiveModel = ActiveModel.with_context(mail_notify_user_signature=False, mail_auto_delete=wizard.template_id.auto_delete)
+            if not hasattr(ActiveModel, 'message_post'):
+                ActiveModel = self.env['mail.thread'].with_context(thread_model=wizard.model)
+            if wizard.composition_mode == 'mass_post':
+                # do not send emails directly but use the queue instead
+                # add context key to avoid subscribing the author
+                ActiveModel = ActiveModel.with_context(mail_notify_force_send=False, mail_create_nosubscribe=True)
+            # wizard works in batch mode: [res_id] or active_ids or active_domain
+            if mass_mode and wizard.use_active_domain and wizard.model:
+                res_ids = self.env[wizard.model].search(safe_eval(wizard.active_domain)).ids
+            elif mass_mode and wizard.model and self._context.get('active_ids'):
+                res_ids = self._context['active_ids']
+            else:
+                res_ids = [wizard.res_id]
+
+            batch_size = int(self.env['ir.config_parameter'].sudo().get_param('mail.batch_size')) or self._batch_size
+            sliced_res_ids = [res_ids[i:i + batch_size] for i in range(0, len(res_ids), batch_size)]
+
+            if (wizard.composition_mode == 'mass_mail' 
+            or wizard.is_log or 
+            (wizard.composition_mode == 'mass_post' and not wizard.notify) 
+            or wizard.is_private):  # log a note: subtype is False
+                subtype_id = False
+            elif wizard.subtype_id:
+                subtype_id = wizard.subtype_id.id
+            else:
+                subtype_id = self.sudo().env.ref('mail.mt_comment', raise_if_not_found=False).id
+
+            for res_ids in sliced_res_ids:
+                batch_mails = Mail
+                all_mail_values = wizard.get_mail_values(res_ids)
+                for res_id, mail_values in all_mail_values.iteritems():
+                    if wizard.composition_mode == 'mass_mail':
+                        batch_mails |= Mail.create(mail_values)
+                    else:
+                        ActiveModel.browse(res_id).message_post(
+                            message_type=wizard.message_type,
+                            subtype_id=subtype_id,
+                            **mail_values)
+
+                if wizard.composition_mode == 'mass_mail':
+                    batch_mails.send(auto_commit=auto_commit)
+
+        return {'type': 'ir.actions.act_window_close'}
--- mail_private/static/src/js/mail_private.js
+++ mail_private/static/src/js/mail_private.js
@@ -3,98 +3,192 @@ odoo.define('mail_private', function (require) {
 
 var core = require('web.core');
 var Chatter = require('mail.Chatter');
+var MailComposer = require('mail_base.base').MailComposer;
 var chat_manager = require('mail.chat_manager');
 var session = require('web.session');
 var Model = require('web.Model');
+var config = require('web.config');
+var utils = require('mail.utils');
 
 
-    Chatter.include({
-        init: function () {
-            this._super.apply(this, arguments);
-            this.private = false;
-            this.events['click .oe_compose_post_private'] = 'on_open_composer_private_message';
-        },
-
-        on_post_message: function (message) {
-        var self = this;
-        if (this.private) {
-            message.subtype = false;
-        }
-        var options = {model: this.model, res_id: this.res_id};
-        chat_manager.post_message(message, options).then(
-            function () {
-                self.close_composer();
-                if (message.partner_ids.length) {
-                    self.refresh_followers();
-                }
-            }).fail(function () {
-                // todo: display notification
-            });
+Chatter.include({
+    init: function () {
+        this._super.apply(this, arguments);
+        this.private = false;
+        this.events['click .oe_compose_post_private'] = 'on_open_composer_private_message';
     },
 
-        on_open_composer_private_message: function (event) {
-            var self = this;
-            this.private = true;
-            this.get_recipients_for_internal_message().then(function (data) {
-                var private_message = 'private_message';
-                self.recipients_for_internal_message = data;
-                self.open_composer(private_message);
-            });
-        },
-
-        on_open_composer_new_message: function () {
-            this._super.apply(this, arguments);
-            this.private = false;
-        },
-
-        open_composer: function (options) {
-            var self = this;
-            this._super.apply(this, arguments);
-            if (options === 'private_message') {
-                //Clear existing suggested partners
-                for (var i=0; i<self.recipients_for_internal_message.length; i++) {
-                    this.composer.suggested_partners.push({
-                        checked: true,
-                        partner_id: self.recipients_for_internal_message[i].id,
-                        full_name: self.recipients_for_internal_message[i].name,
-                        name: self.recipients_for_internal_message[i].name,
-                        email_address: self.recipients_for_internal_message[i].email,
-                        reason: _.include(self.recipients_for_internal_message[i].user_ids, self.session.uid)
-                        ?'Partner'
-                        :'Follower'
-                    });
-                }
+    on_post_message: function (message) {
+    var self = this;
+    if (this.private) {
+        message.subtype = false;
+    }
+    var options = {model: this.model, res_id: this.res_id};
+    chat_manager.post_message(message, options).then(
+        function () {
+            self.close_composer();
+            if (message.partner_ids.length) {
+                self.refresh_followers();
             }
-        },
+        }).fail(function () {
+            // todo: display notification
+        });
+    },
 
-        get_recipients_for_internal_message: function () {
-            var self = this;
-            self.result = {};
-            return new Model(this.context.default_model).query(
-                ['message_follower_ids', 'partner_id']).filter(
-                [['id', '=', self.context.default_res_id]]).all().
-                then(function (thread) {
-                        var follower_ids = thread[0].message_follower_ids;
-                        self.result[self.context.default_res_id] = [];
-                        self.customer = thread[0].partner_id;
-                        // Fetch partner ids
-                        return new Model('mail.followers').call(
-                            'read', [follower_ids, ['partner_id']]).then(function (res_partners) {
-                                var res_partners_filtered = [];
-                                // Filter result and push to array
-                                _.each(res_partners, function (partner) {
-                                    if (partner.partner_id[0] && partner.partner_id[0] !== session.partner_id ) {
-                                        res_partners_filtered.push(partner.partner_id[0]);
-                                    }
-                                });
-                                return new Model('res.partner').call(
-                                    'read', [res_partners_filtered, ['name', 'email', 'user_ids']]
-                                        ).then(function (recipients) {
-                                            return recipients;
-                                        });
-                            });
-            });
+    on_open_composer_private_message: function (event) {
+        var self = this;
+        this.private = true;
+        this.get_recipients_for_internal_message().then(function (data) {
+            self.recipients_for_internal_message = data;
+            self.open_composer({is_private: true});
+        });
+    },
+
+    on_open_composer_new_message: function () {
+        this._super.apply(this, arguments);
+        this.private = false;
+    },
+
+    open_composer: function (options) {
+
+        var self = this;
+        var old_composer = this.composer;
+        // create the new composer
+        this.composer = new MailComposer(this, this.thread_dataset, {
+            commands_enabled: false,
+            context: this.context,
+            input_min_height: 50,
+            input_max_height: Number.MAX_VALUE, // no max_height limit for the chatter
+            input_baseline: 14,
+            is_log: options && options.is_log,
+            record_name: this.record_name,
+            default_body: old_composer && old_composer.$input && old_composer.$input.val(),
+            default_mention_selections: old_composer && old_composer.mention_get_listener_selections(),
+            is_private: options && options.is_private,
+        });
+        this.composer.on('input_focused', this, function () {
+            this.composer.mention_set_prefetched_partners(this.mention_suggestions || []);
+        });
+        this.composer.insertBefore(this.$('.o_mail_thread')).then(function () {
+            // destroy existing composer
+            if (old_composer) {
+                old_composer.destroy();
+            }
+            if (!config.device.touch) {
+                self.composer.focus();
+            }
+            self.composer.on('post_message', self, self.on_post_message);
+            self.composer.on('need_refresh', self, self.refresh_followers);
+            self.composer.on('close_composer', null, self.close_composer.bind(self, true));
+        });
+        this.mute_new_message_button(true);
+
+        if (options && options.is_private) {
+            //Clear existing suggested partners
+            for (var i=0; i<self.recipients_for_internal_message.length; i++) {
+                this.composer.suggested_partners.push({
+                    checked: true,
+                    partner_id: self.recipients_for_internal_message[i].id,
+                    full_name: self.recipients_for_internal_message[i].name,
+                    name: self.recipients_for_internal_message[i].name,
+                    email_address: self.recipients_for_internal_message[i].email,
+                    reason: _.include(self.recipients_for_internal_message[i].user_ids, self.session.uid)
+                    ?'Partner'
+                    :'Follower'
+                });
+            }
         }
-    });
+    },
+
+    get_recipients_for_internal_message: function () {
+        var self = this;
+        self.result = {};
+        return new Model(this.context.default_model).query(
+            ['message_follower_ids', 'partner_id']).filter(
+            [['id', '=', self.context.default_res_id]]).all().
+            then(function (thread) {
+                    var follower_ids = thread[0].message_follower_ids;
+                    self.result[self.context.default_res_id] = [];
+                    self.customer = thread[0].partner_id;
+                    // Fetch partner ids
+                    return new Model('mail.followers').call(
+                        'read', [follower_ids, ['partner_id']]).then(function (res_partners) {
+                            var res_partners_filtered = [];
+                            // Filter result and push to array
+                            _.each(res_partners, function (partner) {
+                                if (partner.partner_id[0] && partner.partner_id[0] !== session.partner_id ) {
+                                    res_partners_filtered.push(partner.partner_id[0]);
+                                }
+                            });
+                            return new Model('res.partner').call(
+                                'read', [res_partners_filtered, ['name', 'email', 'user_ids']]
+                                    ).then(function (recipients) {
+                                        return recipients;
+                                    });
+                        });
+        });
+    }
+});
+
+MailComposer.include({
+    init: function (parent, dataset, options) {
+        this._super(parent, dataset, options);
+        this.events['click .oe_compose_uncheck'] = 'on_uncheck_recipients';
+
+    },
+
+    on_uncheck_recipients: function () {
+        this.$('.o_composer_suggested_partners input:checked').each(function() {
+            $(this).prop('checked', false);
+        });
+    },
+
+    on_open_full_composer: function() {
+        if (!this.do_check_attachment_upload()){
+            return false;
+        }
+
+        var self = this;
+        var recipient_done = $.Deferred();
+        if (this.options.is_log) {
+            recipient_done.resolve([]);
+        } else {
+            var checked_suggested_partners = this.get_checked_suggested_partners();
+            recipient_done = this.check_suggested_partners(checked_suggested_partners);
+        }
+        recipient_done.then(function (partner_ids) {
+            var context = {
+                default_parent_id: self.id,
+                default_body: utils.get_text2html(self.$input.val()),
+                default_attachment_ids: _.pluck(self.get('attachment_ids'), 'id'),
+                default_partner_ids: partner_ids,
+                default_is_log: self.options.is_log,
+                default_is_private: self.options && self.options.is_private,
+                mail_post_autofollow: true,
+            };
+
+            if (self.context.default_model && self.context.default_res_id) {
+                context.default_model = self.context.default_model;
+                context.default_res_id = self.context.default_res_id;
+            }
+
+            self.do_action({
+                type: 'ir.actions.act_window',
+                res_model: 'mail.compose.message',
+                view_mode: 'form',
+                view_type: 'form',
+                views: [[false, 'form']],
+                target: 'new',
+                context: context,
+            }, {
+                on_close: function() {
+                    self.trigger('need_refresh');
+                    var parent = self.getParent();
+                    chat_manager.get_messages({model: parent.model, res_id: parent.res_id});
+                },
+            }).then(self.trigger.bind(self, 'close_composer'));
+        });
+    }
+});
 
 });
