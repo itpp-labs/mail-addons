2dfb242689ec41d21ceac7f7e1690deac3d85316
Aleksandr <kolushov@it-projects.info>
2018-06-07 15:55:30 +0500

    [11.0][PORT] mail_move_message (#146)

---
+++ mail_move_message/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright 2016 Ildar Nasyrov <https://it-projects.info/team/iledarn>
+# Copyright 2017 Ilmir Karamov <https://it-projects.info/team/ilmir-k>
+# Copyright 2017 Lilia Salihova
+# Copyright 2016-2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Mail Relocation',
+    'version': '11.0.1.0.5',
+    'author': 'IT-Projects LLC, Ivan Yelizariev, Pavel Romanchenko',
+    'license': 'LGPL-3',
+    'category': 'Discuss',
+    'images': ['images/m1.png'],
+    "support": "apps@it-projects.info",
+    'website': 'https://twitter.com/yelizariev',
+    'price': 100.00,
+    'currency': 'EUR',
+    'depends': [
+        'mail_all',
+    ],
+    'data': [
+        'mail_move_message_views.xml',
+        'data/mail_move_message_data.xml',
+    ],
+    'qweb': [
+        'static/src/xml/mail_move_message_main.xml',
+    ],
+    'installable': True,
+}
---
+++ mail_move_message/doc/index.rst
@@ -0,0 +1,35 @@
+=================
+ Mail Relocation
+=================
+
+Installation
+============
+
+* `Install <https://odoo-development.readthedocs.io/en/latest/odoo/usage/install-module.html>`__ this module in a usual way
+
+Configuration
+=============
+
+* Open ``[[ Settings ]] >> Mail Relocation`` menu
+* In **Model** field add models to be used for message relocation
+* Check the box **[x] Move Followers** to move followers by default when relocation
+
+Usage
+=====
+
+Move message
+------------
+
+* Open ``[[ Discuss ]] >> Inbox`` menu
+* Click on icon of two cross arrows 
+* Select a record you need 
+* Click **Move** 
+RESULT: The message has been moved to the record selected.
+
+Move to origin
+--------------
+
+* Open the record where the message was moved to
+* Click on the two cross arrows icon highlighted as red
+* Check the box **[x] Move to origin**
+RESULT: The message has been returned back to the original record.
