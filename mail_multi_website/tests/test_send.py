e6ab49e20fa576fe246221f36ba13d4d32575067
Ivan Yelizariev <yelizariev@it-projects.info>
2018-08-21 19:59:02 +0500

    :fire: partner's email must not be multi-website

--- mail_multi_website/tests/test_send.py
+++ mail_multi_website/tests/test_send.py
@@ -19,8 +19,9 @@ class TestSendMail(TransactionCase):
         # Check that current email is set and differs
         self.assertTrue(self.email)
         self.assertNotEqual(self.original_email, self.email)
-
         self.website.company_id = self.company
+
+    def switch_user_website(self):
         # add website to allowed
         self.env.user.write(dict(
             backend_website_ids=[(4, self.website.id)],
@@ -28,10 +29,30 @@ class TestSendMail(TransactionCase):
             company_id=self.company.id,
             company_ids=[(4, self.company.id)]
         ))
-        # update email
-        self.env.user.email = self.email
 
     def test_multi_email(self):
+        """User has email addresses per website"""
+        self.switch_user_website()
+        # update user's email
+        self.env.user.email = self.email
+
         # changing company will automatically update website value to empty value
         self.env.user.company_id = self.original_company
         self.assertEqual(self.env.user.email, self.original_email, 'Multi-email doesn\'t work on switching websites')
+
+    def test_multi_email_partner(self):
+        """Partner doesn't have email addresses per website"""
+        original_email = 'original@email1'
+        new_email = 'new@email2'
+        partner = self.env['res.partner'].create({
+            'name': 'test',
+            'email': original_email,
+        })
+        self.switch_user_website()
+        # update partner's email
+        partner.email = new_email
+        self.assertEqual(partner.email, new_email)
+        # changing company will automatically update website value to empty value
+        self.env.user.company_id = self.original_company
+
+        self.assertEqual(partner.email, new_email, 'Partner\'s email must not be Multi-website')
