2799e60862783fb9d859352b264e35523f7ef04e
ommo73 <rafailov@it-projects.info>
2019-05-21 16:05:34 +0500

    :rainbow: mail_private: refactored

--- mail_private/__manifest__.py
+++ mail_private/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "Discuss",
     # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=12.0",
     "images": [],
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "application": False,
 
     "author": "IT-Projects LLC, Pavel Romanchenko",
