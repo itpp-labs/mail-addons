b6a981ced783d52249f51dcaed7f8a43c30b6a13
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-12 08:54:33 +0000

    :green_heart: fix version format

--- mail_check_immediately/__manifest__.py
+++ mail_check_immediately/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Check mail immediately",
-    "version": "1.0.1",
+    "vesion": "12.0.1.0.1",
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "license": "LGPL-3",
     "category": "Discuss",
