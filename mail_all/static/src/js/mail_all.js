01ca29176bb4ffb13adfd7a35be4bc3f001c7788
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-02-19 13:54:35 +0500

    :rainbow: pre-commit: fix eslint

--- mail_all/static/src/js/mail_all.js
+++ mail_all/static/src/js/mail_all.js
@@ -13,7 +13,6 @@ odoo.define("mail_all.all", function(require) {
 
     Manager.include({
         _updateMailboxesFromServer: function(data) {
-            var self = this;
             this._super(data);
             if (
                 !_.find(this.getThreads(), function(th) {
