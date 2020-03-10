af15961ef7a8d04df08a0ea164fd4d5773c00575
Eugene Molotov <molotov@it-projects.info>
2020-03-10 15:12:18 +0500

    Пожалуй это тоже

--- mail_decode_address/models/mail_thread.py
+++ mail_decode_address/models/mail_thread.py
@@ -8,6 +8,12 @@ class MailThread(models.AbstractModel):
 
     _inherit = 'mail.thread'
 
+    @api.model
+    def create(self, vals):
+        if vals.get("email_from"):
+            vals["email_from"] = tools.decode_smtp_header(vals["email_from"])
+        return super(MailThread, self).create(vals)
+
     def _message_post_after_hook(self, message, msg_vals):
         if message.email_from:
             message.email_from = tools.decode_smtp_header(message.email_from)
