fff8adcb4c63ef11f5168c306f450cefb3d88bc4
Vildan Safin <safin@it-projects.info>
2020-07-30 16:48:55 +0500

    Merge remote-tracking branch 'Eugene/12.0-mail_addons-merge-mail_to-fix' into 12.0-mail_to
    Signed-off-by: Vildan Safin <safin@it-projects.info>

--- mail_to/tests/test_default.py
+++ mail_to/tests/test_default.py
@@ -1,6 +1,9 @@
-from werkzeug import url_encode
-
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2018 Artyom Losev <https://it-projects.info/team/ArtyomLosev>
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl.html).
 import odoo.tests
+from werkzeug import url_encode
 
 
 @odoo.tests.common.at_install(True)
