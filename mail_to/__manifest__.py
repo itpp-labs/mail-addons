b49fe92d31d9591471c2b21edcab35ecbb6f5929
Ivan Yelizariev <iel@odoo.com>
2020-12-06 14:54:37 +0100

    :ambulance: mail_to: error on sending bot commands
    fixes #317

--- mail_to/__manifest__.py
+++ mail_to/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2016 x620 <https://github.com/x620>
-# Copyright 2016 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2016,2020 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # Copyright 2018 Ruslan Ronzhin
 # Copyright 2019 Artem Rafailov <https://it-projects.info/team/Ommo73/>
 # License LGPL-3.0 (https://www.gnu.org/licenses/lgpl.html).
@@ -8,7 +8,7 @@
     "summary": """Allows you be sure, that all discussion participants were notified""",
     "category": "Discuss",
     "images": ["images/1.png"],
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "support": "apps@itpp.dev",
     "website": "https://it-projects.info",
