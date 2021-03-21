e4f82270402e62884dc802dedf8d163f27b271e5
Ivan Yelizariev <iel@odoo.com>
2021-03-21 13:39:03 +0100

    :book: new website

--- mail_archives/__manifest__.py
+++ mail_archives/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "12.0.1.0.1",
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "support": "apps@itpp.dev",
-    "website": "https://it-projects.info",
+    "website": "https://itpp.dev",
     "license": "Other OSI approved licence",  # MIT
     "price": 40.00,
     "currency": "EUR",
