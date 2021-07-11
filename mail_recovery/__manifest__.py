d51c607696523ee49b8797bfc6a44734a818faef
Ivan Yelizariev <iel@odoo.com>
2021-07-11 17:14:09 +0200

    :book: fix typo in version tag

--- mail_recovery/__manifest__.py
+++ mail_recovery/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://twitter.com/nasyrov_ildar",
     "category": "Discuss",
     "images": ["images/mail_recovery.png"],
-    "vesion": "11.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": ["mail"],
     "data": ["data.xml"],
     "installable": False,
